05f01cdae8217e3de81b4f6ecb454d1ec5d14413
KwonYiHyun <diamoboy1@gmail.com>
2023-01-16 17:42:51 +0900

    Algorithm-01-16
    백준 1912(연속합) c++

--- Backjoon/1912_연속합.cpp
+++ Backjoon/1912_연속합.cpp
@@ -1,7 +1,8 @@
-// 2023-01-13
+// 2023-01-16
 // 백준 1912
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -10,10 +11,24 @@ int main(){
     cin >> n;
 
     int *arr = new int[n];
+    int *result = new int[n]();
+
     for (int i = 0; i < n; i++)
-    {
         cin >> arr[i];
+
+    result[0] = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] <= result[i - 1] + arr[i])
+            result[i] = result[i - 1] + arr[i];
+        else
+            result[i] = arr[i];
+
+        if (min < result[i])
+            min = result[i];
     }
-    
-    
+
+    cout << min;
 }
